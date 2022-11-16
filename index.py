4e3e58845ecbef6e444b1f583a2503afda8b2d5b
NghiaPP <phunghia91bkdn@gmail.com>
2022-11-16 17:33:32 +0700

    get top coin

--- index.py
+++ index.py
@@ -3,17 +3,37 @@ import indicators as indis
 from trending import Trending
 import alert_strategy as Alert
 from util import print_json
+from constants import *
 
 
 def main():
-    symbol = 'BTCUSDT'
+    show_trend()
+
+
+def show_trend():
+    symbol = 'ETHUSDT'
     interval = Client.KLINE_INTERVAL_1HOUR
     print(f'Current price: {indis.current_price(symbol)}')
-    ema_trend = Alert.ma_trending_prime_ma_for_1_4_1(symbol)
+    ema_trend = Alert.ma_trending_prime_ma_for_15_1_4(symbol)
     print_json(ema_trend["trend_consensus"])
     print(
         f'Prime ema: {ema_trend["prime_ema"]["ema"]}, Current value = {ema_trend["prime_ema"]["current_value"]}, Trust percent = {ema_trend["prime_ema"]["percent"]}, Price often {ema_trend["prime_ema"]["trend"]}')
     print(f'RSI: {ema_trend["rsi"]}, ATR: {ema_trend["atr"]}')
+
+
+def get_top_10():
+    lis_coin_with_USDT = list(map(lambda x: x["symbol"], list(filter(lambda x: x["quoteAsset"] ==
+                                                                     "USDT", client.get_exchange_info()["symbols"]))))
+    # print_json(info)
+    # avg_price = client.get_avg_price(symbol='BNBUSDT')
+    # print(avg_price)
+    info_24h = []
+    for coin in lis_coin_with_USDT:
+        info_24h.append(client.get_ticker(symbol=coin))
+
+    top_10 = sorted(info_24h, key=lambda x: float(
+        x["quoteVolume"]), reverse=True)[:20]
+    print_json(top_10)
 
 
 if __name__ == "__main__":
--- indicators.py
+++ indicators.py
@@ -2,14 +2,14 @@ import pandas as pd
 import pandas_ta as ta
 from binance.client import Client
 
-import constants as cons
+from constants import *
 from model.kline import Kline
 
 
 def calc_current_ema(symbol='BTCBUSD', kline_interval=Client.KLINE_INTERVAL_1DAY, length=34, data_frame=None):
     if data_frame is None:
-        data_frame = pd.DataFrame(cons.client.get_historical_klines(
-            symbol, kline_interval, limit=1000), columns=cons.KLINE_FORMAT)
+        data_frame = pd.DataFrame(client.get_historical_klines(
+            symbol, kline_interval, limit=1000), columns=KLINE_FORMAT)
 
     ret = ta.ema(pd.to_numeric(
         data_frame["close_price"], downcast="float"), length=length)
@@ -18,8 +18,8 @@ def calc_current_ema(symbol='BTCBUSD', kline_interval=Client.KLINE_INTERVAL_1DAY
 
 def calc_1000_ema(symbol='BTCBUSD', kline_interval=Client.KLINE_INTERVAL_1DAY, length=34, data_frame=None):
     if data_frame is None:
-        data_frame = pd.DataFrame(cons.client.get_historical_klines(
-            symbol, kline_interval, limit=1000), columns=cons.KLINE_FORMAT)
+        data_frame = pd.DataFrame(client.get_historical_klines(
+            symbol, kline_interval, limit=1000), columns=KLINE_FORMAT)
     ret = ta.ema(pd.to_numeric(
         data_frame["close_price"], downcast="float"), length=length)
     return pd.to_numeric(data_frame["close_price"]), ret
@@ -27,8 +27,8 @@ def calc_1000_ema(symbol='BTCBUSD', kline_interval=Client.KLINE_INTERVAL_1DAY, l
 
 def calc_current_rsi(symbol='BTCBUSD', kline_interval=Client.KLINE_INTERVAL_1DAY, length=14, data_frame=None):
     if data_frame is None:
-        data_frame = pd.DataFrame(cons.client.get_historical_klines(
-            symbol, kline_interval, limit=1000), columns=cons.KLINE_FORMAT)
+        data_frame = pd.DataFrame(client.get_historical_klines(
+            symbol, kline_interval, limit=1000), columns=KLINE_FORMAT)
     ret = ta.rsi(pd.to_numeric(
         data_frame["close_price"], downcast="float"), length=length)
     return round(ret[len(ret) - 1], 2)
@@ -36,8 +36,8 @@ def calc_current_rsi(symbol='BTCBUSD', kline_interval=Client.KLINE_INTERVAL_1DAY
 
 def calc_1000_rsi(symbol='BTCBUSD', kline_interval=Client.KLINE_INTERVAL_1DAY, length=34, data_frame=None):
     if data_frame is None:
-        data_frame = pd.DataFrame(cons.client.get_historical_klines(
-            symbol, kline_interval, limit=1000), columns=cons.KLINE_FORMAT)
+        data_frame = pd.DataFrame(client.get_historical_klines(
+            symbol, kline_interval, limit=1000), columns=KLINE_FORMAT)
     ret = ta.rsi(pd.to_numeric(
         data_frame["close_price"], downcast="float"), length=length)
     return pd.to_numeric(data_frame["close_price"]), ret
@@ -45,8 +45,8 @@ def calc_1000_rsi(symbol='BTCBUSD', kline_interval=Client.KLINE_INTERVAL_1DAY, l
 
 def calc_current_atr(symbol='BTCBUSD', kline_interval=Client.KLINE_INTERVAL_1DAY, length=14, mamode='rma', data_frame=None):
     if data_frame is None:
-        data_frame = pd.DataFrame(cons.client.get_historical_klines(
-            symbol, kline_interval, limit=1000), columns=cons.KLINE_FORMAT)
+        data_frame = pd.DataFrame(client.get_historical_klines(
+            symbol, kline_interval, limit=1000), columns=KLINE_FORMAT)
     ret = ta.atr(pd.to_numeric(
         data_frame["high_price"], downcast="float"), pd.to_numeric(
         data_frame["low_price"], downcast="float"), pd.to_numeric(
@@ -55,5 +55,5 @@ def calc_current_atr(symbol='BTCBUSD', kline_interval=Client.KLINE_INTERVAL_1DAY
 
 
 def current_price(symbol):
-    return Kline(cons.client.get_historical_klines(
+    return Kline(client.get_historical_klines(
         symbol, interval=Client.KLINE_INTERVAL_30MINUTE, limit=1)[0]).close_price
